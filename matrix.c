bc7f5304019fdcf696fd6cd7daa27404db0b487e
Vinicius <vinioliveira.s@hotmail.com>
2022-08-08 19:21:45 -0300

    (add) conding files

---
+++ matrix.c
@@ -0,0 +1,154 @@
+#include "matrix.h"
+
+double** newMatrix(int l, int c)
+{
+    double **M;
+       M = malloc (l * sizeof (double*)) ;
+    int i;
+    for (i=0; i < l; i++)
+        M[i] = malloc (c * sizeof (double)) ;
+    return M;
+}
+
+void freeMatrix(double **M, int l)
+{
+    int i;
+    for (i = 0; i < l; i++)
+        free(M[i]);
+    free(M);
+}
+
+void printMatrix(double **M, int l, int c)
+{
+    int i, j;
+    for (i = 0; i < l; i++)
+    {
+        printf("| ");
+        for(j = 0; j < c; j++ )
+            printf("%14.5lf ", M[i][j]);
+        printf("|\n");
+    }
+    printf("\n");
+}
+
+void fillMatrix(double **M, int l, int c, bool seed)
+{
+    int i,j;
+    clock_t clk = clock();
+    if (seed)
+        srand(clk);
+    for (i = 0; i < l; i++)
+        for (j = 0; j < c; j++)
+            M[i][j] = ((double)rand() * 1000.0) / ((double)RAND_MAX);
+}
+
+double** matrixSquare(double **M, int l, int c)
+{
+    double **M2 = newMatrix(l, c);
+    int i, j;
+    for (i = 0; i < l; i++)
+        for ( j = 0; j < c; j++)
+            M2[i][j] = M[i][j] * M[i][j];
+    return M2;
+}
+
+double** twoMatrixSub(double **A, double**B, int l, int c)
+{
+    double **C = newMatrix(l, c);
+    int i, j;
+    for (i = 0; i < l; i++)
+        for ( j = 0; j < c; j++)
+            C[i][j] = A[i][j] - B[i][j];
+    return C;
+}
+
+double matrixSum(double **M, int l, int c)
+{
+    int i, j;
+    double sum = 0.0;
+    for (i = 0; i < l; i++)
+        for ( j = 0; j < c; j++)
+            sum += M[i][j];
+    return sum;
+}
+
+void exportToFile(double **M, int l, int c, char *Matrix, char *filename, bool csv)
+{
+    FILE *file;
+    file = fopen(filename,"a");
+
+    int i,j;
+    char buffer[100];
+
+    fputs(Matrix, file);
+    fputc('\n', file);
+
+    for(i = 0; i < l; i++)
+    {
+        for (j = 0; j < c; j++)
+        {
+            sprintf(buffer, "%lf", M[i][j]);
+            fputs(buffer, file);
+            if(csv)
+                fputc(',',file);
+            else
+                fputc(' ',file);
+        }
+        fputc('\n', file);
+    }
+    fputc('\n',file);
+    fclose(file);
+}
+
+void fillMatrix_P(double **M, int l, int c, bool seed)
+{
+    int i,j;
+    clock_t clk = clock();
+    omp_set_num_threads(NUM_THREADS);
+    #pragma omp parallel shared(i, M, clk) private(j)
+    if (seed)
+        srand(clk);
+    #pragma omp for
+    for (i = 0; i < l; i++)
+        for (j = 0; j < c; j++)
+            M[i][j] = ((double)rand() * 1000.0) / ((double)RAND_MAX);
+}
+
+double** matrixSquare_P(double **M, int l, int c)
+{
+    double **M2 = newMatrix(l, c);
+    int i, j;
+    omp_set_num_threads(NUM_THREADS);
+    #pragma omp parallel shared(i, M2, M) private(j)
+    #pragma omp for
+    for (i = 0; i < l; i++)
+        for ( j = 0; j < c; j++)
+            M2[i][j] = M[i][j] * M[i][j];
+    return M2;
+}
+
+double** twoMatrixSub_P(double **A, double**B, int l, int c)
+{
+    double **C = newMatrix(l, c);
+    int i, j;
+    omp_set_num_threads(NUM_THREADS);
+    #pragma omp parallel shared(i, C, A, B) private(j)
+    #pragma omp for
+    for (i = 0; i < l; i++)
+        for ( j = 0; j < c; j++)
+            C[i][j] = A[i][j] - B[i][j];
+    return C;
+}
+
+double matrixSum_P(double **M, int l, int c)
+{
+    int i, j;
+    double sum = 0.0;
+    omp_set_num_threads(NUM_THREADS);
+    #pragma omp parallel shared(M, i) private(j) reduction(+:sum)
+    #pragma omp for
+    for (i = 0; i < l; i++)
+        for ( j = 0; j < c; j++)
+            sum += M[i][j];
+    return sum;
+}
---
+++ sdqm.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <string.h>
+#include "matrix.h"
+#include "omp.h"
+
+int main(int argc, char *argv[])
+{
+    // Verifica a chamada de execução do programa
+    if (argc < 4 || argc > 5)
+    {
+        printf("Invalid format!\n");
+        printf("./sdqm.x l c p|s\n");
+        return 0;
+    }
+    int l, c;
+    char *mode;
+    l = atoi(argv[1]);
+    c = atoi(argv[2]);
+    mode = argv[3];
+    if (strcmp(mode,"p") && strcmp(mode,"s"))
+    {
+        printf("Invalid format!\n");
+        printf("./sdqm.x l c p|s\n");
+        return 0;
+    }
+
+    //Aloca e gera duas matrizes com valores aleatorios
+    double **A, **A2, **B, **B2, **C, D = 0.0;
+    A = A2 = B = B2 = C = NULL;
+    A = newMatrix(l, c);
+    B = newMatrix(l, c);
+        
+    double time = 0.0;
+    
+    // Modo sequencial
+    if (!strcmp(mode, "s"))
+    {
+        clock_t begin, end;
+        //preenche as matrizes
+        begin = clock();
+        fillMatrix(A, l, c, false);
+        fillMatrix(B, l, c, false);
+        A2 = matrixSquare(A, l, c);
+        B2 = matrixSquare(B, l, c);
+        C = twoMatrixSub(A2, B2, l, c);
+        D = matrixSum(C, l, c);
+        end = clock();
+        time = (float)(((end - begin) + 0.0) / CLOCKS_PER_SEC);
+    }
+    // Modo paralelo
+    else if (!strcmp(mode, "p"))
+    {
+        double begin, end;
+        begin = omp_get_wtime();
+        fillMatrix_P(A, l, c, false);
+        fillMatrix_P(B, l, c, false);
+        A2 = matrixSquare_P(A, l, c);
+        B2 = matrixSquare_P(B, l, c);
+        C = twoMatrixSub_P(A2, B2, l, c);
+        D = matrixSum_P(C, l, c);
+        end = omp_get_wtime();
+        time = end - begin;
+    }
+    
+    //printa as matrizes no terminal
+    if (argc == 5 && strcmp("print", argv[4]) == 0)
+    {
+        printf("Matriz A: \n");
+        printMatrix(A, l, c);
+        printf("Quadrado da Matriz A: \n");
+        printMatrix(A2, l, c);
+        printf("Matriz B: \n");
+        printMatrix(B, l, c);
+        printf("Quadrado da Matriz B: \n");
+        printMatrix(B2, l, c);
+        printf("Matriz das diferenças (A-B): \n");
+        printMatrix(C, l, c);
+    }
+
+    //grava as matrizes em um arquivo 
+    else if (argc == 5 && strcmp("txt", argv[4]) == 0)
+    {
+        exportToFile(A, l, c, "Matriz A", "matriz.txt", false);
+        exportToFile(A, l, c, "Quadrado Matriz A", "matriz.txt", false);
+        exportToFile(B, l, c, "Matriz B", "matriz.txt", false);
+        exportToFile(B2, l, c, "Quadrado Matriz B2", "matriz.txt", false);
+        exportToFile(C, l, c, "Matriz diferença dos quadrados", "matriz.txt", false);
+    }
+
+    else if (argc == 5 && strcmp("csv", argv[4]) == 0)
+    {
+        exportToFile(A, l, c, "Matriz A", "matriz.csv", true);
+        exportToFile(A, l, c, "Quadrado Matriz A", "matriz.csv", true);
+        exportToFile(B, l, c, "Matriz B", "matriz.csv", true);
+        exportToFile(B2, l, c, "Quadrado Matriz B2", "matriz.csv", true);
+        exportToFile(C, l, c, "Matriz diferença dos quadrados", "matriz.csv", true);
+    }
+
+    // printa apenas o tempo de execução
+    if (argc == 5 && strcmp("out", argv[4]) == 0)
+        printf("%f\n", time);
+    else
+    { 
+        printf("Tempo de execucao = %f\n", time);
+        printf("Soma das difereças = %f\n", D);
+    }
+
+    //desaloca as matrizes utillizadas
+    if (A!=NULL)
+        freeMatrix(A,l);
+    if (A2!=NULL)
+        freeMatrix(A2,l);
+    if (B!=NULL)
+        freeMatrix(B,l);
+    if (B2!=NULL)
+        freeMatrix(B2,l);
+    if (C!=NULL)
+        freeMatrix(C,l);
+    return 0;
+}
